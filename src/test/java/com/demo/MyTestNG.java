447b84f62739f44596710a082241a0131f2a5ff8
Bhargodev Arya <bhargodevarya@gmail.com>
2020-09-11 18:40:46 +0530

    Spring test dependency and reading value from yml

--- src/test/java/com/demo/MyTestNG.java
+++ src/test/java/com/demo/MyTestNG.java
@@ -1,13 +1,26 @@
 package com.demo;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 @Test(groups = {"unittest"})
-public class MyTestNG {
+@EnableConfigurationProperties
+@ConfigurationProperties
+@ContextConfiguration(initializers = ConfigFileApplicationContextInitializer.class)
+public class MyTestNG extends AbstractTestNGSpringContextTests {
+
+    @Value("${com.test}")
+    private String dummy;
 
     @Test
     void firstTest() {
+        System.out.println("???? " + dummy);
         Assert.assertEquals("2", new Integer(2).toString());
     }
 }
